4ee7eb8bca3717fded456f4cc0bc3322588d1568
bemorales <bemorales@scl.edu.gt>
2024-07-11 11:23:30 -0600

    rama header

--- header/header.js
+++ header/header.js
@@ -2,5 +2,5 @@ let dom_header = document.querySelector(".header");
 
 dom_header.innerHTML=`
 <div class="bloque">@bemorales</div>
-<div class="bloque1">github</div>
+<a href="https://github.com/bemmoralesmora/Examen-P2" class="bloque1">github</a>
 `;
